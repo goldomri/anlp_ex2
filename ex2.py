b9a40d9d8cc64183ea58db842ae06f973b5f6f5a
goldomri <goldomri@gmail.com>
2025-05-23 17:20:54 +0300

    add testing prompts

--- ex2.py
+++ ex2.py
@@ -5,13 +5,13 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Mistral initialization
 mistral_name = "mistralai/Mistral-7B-Instruct-v0.3"
-mistral_tokenizer   = AutoTokenizer.from_pretrained(mistral_name)
-mistral_model       = AutoModelForCausalLM.from_pretrained(mistral_name, torch_dtype=torch.float16, device_map="auto").eval()
+mistral_tokenizer = AutoTokenizer.from_pretrained(mistral_name)
+mistral_model = AutoModelForCausalLM.from_pretrained(mistral_name, torch_dtype=torch.float16, device_map="auto").eval()
 
 # Llama initialization
 llama_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-llama_tokenizer   = AutoTokenizer.from_pretrained(llama_name)
-llama_model       = AutoModelForCausalLM.from_pretrained(llama_name, torch_dtype=torch.float16, device_map="auto").eval()
+llama_tokenizer = AutoTokenizer.from_pretrained(llama_name)
+llama_model = AutoModelForCausalLM.from_pretrained(llama_name, torch_dtype=torch.float16, device_map="auto").eval()
 
 # Create prompts
 prompts = [
@@ -22,7 +22,8 @@ prompts = [
     "The engineers called the contractors because they had missed the deadline. Who had missed the deadline?"
 ]
 
-def ask_model(model, tokenizer, prompt, max_new_tokens:int = 128):
+
+def ask_model(model, tokenizer, prompt, max_new_tokens: int = 128):
     """
     Return the raw text that the model generates for a single prompt.
     :param model: model to generate text with.
@@ -33,11 +34,23 @@ def ask_model(model, tokenizer, prompt, max_new_tokens:int = 128):
     """
     tokens = tokenizer(prompt, return_tensors="pt").to(device)
     with torch.inference_mode():
-        out = model.generate(**tokens,max_new_tokens=max_new_tokens)
+        out = model.generate(**tokens, max_new_tokens=max_new_tokens)
     return tokenizer.decode(out[0], skip_special_tokens=True).strip()
+
 
 # Generate output of Mistral
 for prompt in prompts:
+    print(ask_model(mistral_model, mistral_tokenizer, prompt))
+    print()
+
+# Test hypothesis
+test_prompts = [
+    "Because his was brand-new, Peter borrowed Tom's bike. Whose bike was brand-new?",
+    "Peter was lent Tom's bike because his was broken. Whose bike was broken?",
+    "Olivia was handed the microphone by Carla because she had a louder voice. Who had a louder voice?"
+]
+
+for prompt in test_prompts:
     print(ask_model(mistral_model, mistral_tokenizer, prompt))
     print()
 
@@ -45,5 +58,3 @@ for prompt in prompts:
 for prompt in prompts:
     print(ask_model(llama_model, llama_tokenizer, prompt))
     print()
-
-
